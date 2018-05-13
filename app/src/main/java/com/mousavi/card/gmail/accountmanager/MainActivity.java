319e305d97a552a8009dd1bc2b9bcd3ea36dd965
Unknown <krsh.mousavi.dev@gmail.com>
2018-05-13 12:17:25 +0430

    firtCommit

---
+++ app/src/main/java/com/mousavi/card/gmail/accountmanager/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mousavi.card.gmail.accountmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mousavi.card.gmail.accountmanager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+  
+  
+}
---
+++ app/src/main/java/com/mousavi/card/gmail/accountmanager/SS.java
@@ -0,0 +1,21 @@
+package com.mousavi.card.gmail.accountmanager;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+//import com.mousavi.card.gmail.easyaccount.EasyAccountService;
+
+public class SS extends Service {
+  @Nullable
+  @Override
+  public IBinder onBind(Intent ibinderIntent) {
+//    EasyAccountService easyAccount= EasyAccountService.newBuilder(this)
+//        .accountType("com.mousavi.card.gmail.accountmanager")
+//        .destinationClass(MainActivity.class)
+//        .enableLogger(true)
+//        .build();
+//    return easyAccount.getIBinder();
+    return null;}
+    
+}
